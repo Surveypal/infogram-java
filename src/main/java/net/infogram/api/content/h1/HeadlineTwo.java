864279ed1e4066d1de42542f71fdfd5d5baffb45
JorgeGarciaI <jorge.garcia@surveypal.com>
2016-01-07 18:40:15 +0200

    Development Added JSON bindings for Content JSON serialization

---
+++ src/main/java/net/infogram/api/content/h1/HeadlineTwo.java
@@ -0,0 +1,64 @@
+package net.infogram.api.content.h1;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import net.infogram.api.content.InfographicElement;
+
+import javax.annotation.Generated;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+	                   "type",
+	                   "text"
+                   })
+public class HeadlineTwo extends InfographicElement
+{
+
+	final static String TYPE = "h2";
+
+	@JsonProperty("type")
+	private String type = TYPE;
+	@JsonProperty("text")
+	private String text;
+
+	/**
+	 * @return The type
+	 */
+	@JsonProperty("type")
+	public String getType()
+	{
+		return type;
+	}
+
+	/**
+	 * @param type The type
+	 */
+	@JsonProperty("type")
+	private HeadlineTwo setType(String type)
+	{
+		this.type = type;
+		return this;
+	}
+
+	/**
+	 * @return The text
+	 */
+	@JsonProperty("text")
+	public String getText()
+	{
+		return text;
+	}
+
+	/**
+	 * @param text The text
+	 */
+	@JsonProperty("text")
+	public HeadlineTwo setText(String text)
+	{
+		this.text = text;
+		return this;
+	}
+
+}
